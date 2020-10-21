e202e0e784cd595e208d03a349143dcae2db0578
chenhaojie <a18520145493@163.com>
2020-10-21 12:43:05 +0800

    update columnDetail page

--- zhihu/src/components/PostList.vue
+++ zhihu/src/components/PostList.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="post-list">
-    <article v-for="item in list" :key="item.id" class="card mb-3 shadow-sm">
+    <article v-for="item in list" :key="item._id" class="card mb-3 shadow-sm">
       <div class="card-body">
         <h4>
           {{item.title}}
         </h4>
         <div class="row my-3 align-items-center">
-          <div v-if="item.image" class="col-4">
+          <div v-if="item.image" class="col-3">
             <img
-              :src="item.image"
+              :src="item.image && item.image.url"
               :alt="item.title"
               class="rounded-lg w-100"
             />
           </div>
           <p :class="{ 'col-8': item.image }" class="text-muted">
-            {{ item.content }}
+            {{ item.excerpt }}
           </p>
         </div>
-        <span class="text-muted">{{ item.createdAt }}</span>
+        <span class="text-muted">{{ item.createAt }}</span>
       </div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from "vue";
-import { PostProps } from "../testData";
+import {  defineComponent, PropType } from "vue";
+import {PostProps} from "../store";
 export default defineComponent({
   name: "PostList",
   props: {
@@ -34,9 +34,15 @@ export default defineComponent({
       required: true,
     },
   },
-  
+  setup(){
+    return {}
+  }
 });
 </script>
 
 <style scoped>
+.card-body img {
+  width: 100px;
+  height: 100px;
+}
 </style>
